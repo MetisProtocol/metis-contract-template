7ebeed973731613b08f4f18f8d1ea33ab90afe4e
Rami Husami <husamirami@gmail.com>
2023-08-08 22:28:53 +0300

    add deployments

--- deploy/metis/001_deploy_simple_erc20.ts
+++ deploy/metis/001_deploy_simple_erc20.ts
@@ -6,14 +6,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const {deployments, getNamedAccounts} = hre;
 	const {deploy} = deployments;
 
-	const {deployer, simpleERC20Beneficiary} = await getNamedAccounts();
+	const {deployer} = await getNamedAccounts();
 
-	await deploy('SimpleERC20', {
+	await deploy('VotingToken', {
 		from: deployer,
-		args: [simpleERC20Beneficiary, parseEther('1000000000')],
+		args: [],
 		log: true,
 		autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
 	});
 };
 export default func;
-func.tags = ['SimpleERC20'];
+func.tags = ['VotingToken'];
